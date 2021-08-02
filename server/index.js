81bccb53e1db97f9b2b2e10687e7ab7d2ae14632
Thongtong <THONGTONG.tg@mail.kmutt.ac.th>
2021-08-02 21:08:39 +0700

    [add] get add update delete

--- server/index.js
+++ server/index.js
@@ -1,14 +1,74 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql')
-const cors = require('cors')
+const cors = require('cors');
+const e = require('cors');
 
 app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-  user :"root",
-  host : "localhost",
-  password : "",
-  database : "employee",
+  user: "root",
+  host: "localhost",
+  password: "",
+  database: "employeeSystem",
+});
+
+////get info 
+app.get('/employees', (req, res) => {
+  db.query("Select * from Employees ", (err, result) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+//post method
+app.post('/create', (req, res) => {
+  const name = req.body.name;
+  const age = req.body.age;
+  const country = req.body.country;
+  const position = req.body.position;
+  const wage = req.body.wage;
+  db.query("insert into employees (name , age , country, position , wage) values (?,?,?,?,?)", [name, age, country, position, wage], (err, result) => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.send("create successful");
+    }
+  });
+
+})
+
+///////update 
+
+app.put ('/update', (req, res)=>{
+  const id = req.body.id;
+  const wage = req.body.wage;
+  db.query("update employees set wage=? where id = ?  " , [wage ,id] ,(err, result) => {
+    if (err) {
+      console.log(err)
+    }else{
+      res.send(result);
+    }
+  })
+})
+
+//// delete 
+
+app.delete("/delete/:id", (req, res) => {
+  const id = req.params.id;
+  db.query("DELETE FROM employees WHERE id = ?", id, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.listen(3001, () => {
+  console.log('Server is running on port 3001')
 });
